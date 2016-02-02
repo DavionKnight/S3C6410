9790d815a0e759bedb203090edeb7225d348466c
kevin <812964073@qq.com>
2016-02-02 17:15:17 +0800

    smdk6410_config compile OK

--- u-boot-2010.03/build.sh
+++ u-boot-2010.03/build.sh
@@ -1,5 +1,10 @@
 export ARCH=arm
 export CROSS_COMPILE=/home/kevin/works/projects/S3C6410/arm-2014.05/bin/arm-none-linux-gnueabi-
-make smdk6400_config
+
+make distclean
+make clean
+
+make smdk6410_config
 make
+
 
