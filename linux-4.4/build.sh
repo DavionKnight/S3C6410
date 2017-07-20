8e462751ca775fafa31ff0397c4c70384562e0f0
kevin <812964073@qq.com>
2017-07-20 16:36:31 +0800

    add tools

---
+++ linux-4.4/build.sh
@@ -0,0 +1,12 @@
+export ARCH=arm
+export PATH=$PATH:/home/kevin/works/projects/S3C6410/arm-2014.05/bin
+export CROSS_COMPILE=/home/kevin/works/projects/S3C6410/arm-2014.05/bin/arm-none-linux-gnueabi-
+
+#make distclean
+#make clean
+
+#make menuconfig
+make
+make uImage
+
+
